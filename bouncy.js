5ec8aee72b10ae4be040a80859c75615e1300b0a
Max Z <max.zlotskiy@gmail.com>
2018-03-10 14:22:52 -0500

    made radius proportional to the speed of the ball

--- bouncy.js
+++ bouncy.js
@@ -1,16 +1,23 @@
 //Add an event that creates a bouncing circle whenever the svg element is clicked
 document.getElementById("svg").addEventListener("click", function(evt) {
+    //make variables for dimensions of the svg container
+    var WIDTH = parseInt(this.getAttribute("width"));
+    var HEIGHT = parseInt(this.getAttribute("height"));
+
     //Initialize velocity to a random direction
-    var velocityX = (Math.random() - 0.5) * 5;
-    var velocityY = (Math.random() - 0.5) * 5;
+    var velocityX = (Math.random() - 0.5) * 10;
+    var velocityY = (Math.random() - 0.5) * 10;
 
     //Initialize position to be the spot where mouse clicked
     var x = evt.offsetX;
     var y = evt.offsetY;
 
+    //Make the radius inversley proportional to how fast the circle is moving
+    var r = 20 - 2*Math.sqrt(velocityX*velocityX + velocityY*velocityY);
+
     //Create the circle element
     var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    circle.setAttribute("r", "10");
+    circle.setAttribute("r", r);
     circle.setAttribute("cx", x);
     circle.setAttribute("cy", y);
     circle.setAttribute("fill", "#82CAFF");
@@ -19,7 +26,8 @@ document.getElementById("svg").addEventListener("click", function(evt) {
     this.appendChild(circle);
 
     //create the animation loop, runs 30/second
-    var id = setInterval(function() {}, 1000 / 30);
+    var id = setInterval(function() {
+    }, 1000 / 30);
 
     //tell the clear button to stop this animation loop when it is clicked
     document.getElementById("clear").addEventListener("click", function(evt) {});
